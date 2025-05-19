2291e2fa11a7ae6f786103e51eab0d4f4482b546
ovvladimir <ov.vladimir@mail.ru>
2025-05-19 23:18:35 +1000

    -

--- Servers/crypto_tracker/handlers/telegram.py
+++ Servers/crypto_tracker/handlers/telegram.py
@@ -2,5 +2,5 @@ import telebot
 
 
 def send_notify(message):
-    bot = telebot.TeleBot("5821284135:AAHpPGbrqy4412hSsB2NjvO3Pib7KzwMZYI")
+    bot = telebot.TeleBot("token")
     bot.send_message(1791124119, message)
