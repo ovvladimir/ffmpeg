935c5c4981f0b7e29a4086b60c4886bfdc4731ec
Vladimir Ovsyannikov <ov.vladimir@mail.ru>
2025-05-20 12:17:34 +1000

    -

--- Servers/polls/aiohttpdemo_polls/main.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-from settings import config
-from routes import setup_routes
-
-app = web.Application()
-setup_routes(app)
-app['config'] = config
-web.run_app(app)  # , host='127.0.0.1', port=1234)
